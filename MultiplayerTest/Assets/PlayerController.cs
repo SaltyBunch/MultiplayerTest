f81f442085f81354898ccec6744029a0e697f6ea
HiRoS-neko <hiros.neko@gmail.com>
2018-09-28 13:58:40 -0400

    Full Working

---
+++ MultiplayerTest/Assets/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody _rgd;
+
+
+    private Vector3 _dir;
+
+    // Use this for initialization
+    void Start()
+    {
+        _rgd = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _dir = (Input.GetAxis("Horizontal") * Vector3.right + Input.GetAxis("Vertical") * Vector3.forward).normalized;
+    }
+
+    private void FixedUpdate()
+    {
+        _rgd.velocity = _dir;
+    }
+}
